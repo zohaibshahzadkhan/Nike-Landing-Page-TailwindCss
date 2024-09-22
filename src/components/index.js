5927c769a9607f7c33a0196ace6787d0e13860bc
Zohaib Shahzad <zohaibshahzad16@gmail.com>
2024-09-23 00:36:03 +0200

    Add customer review section

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Nav from "./Nav";
 import PopularProductCard from "./PopularProductCard";
 import ShoeCard from "./ShoeCard";
 import ServiceCard from "./ServiceCard";
+import ReviewCard from "./ReviewCard";
 
-export { Button, Nav, ShoeCard, PopularProductCard, ServiceCard };
+export { Button, Nav, ShoeCard, PopularProductCard, ServiceCard, ReviewCard };
